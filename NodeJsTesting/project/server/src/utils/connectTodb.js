117a3f533c9cdef8b0efc85c7d12be7b2d581b36
Your Name <you@example.com>
2024-09-11 10:12:02 +0300

    try to update node_modulse

--- NodeJsTesting/project/server/src/utils/connectTodb.js
+++ NodeJsTesting/project/server/src/utils/connectTodb.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 
 async function  connectToDb() {
-    const uri = "mongodb+srv://eyal:eyal123456@e-commerce.fsngodq.mongodb.net/test2"
+    // const uri = "mongodb+srv://eyal:eyal123456@e-commerce.fsngodq.mongodb.net/test2"
+    const uri = "mongodb+srv://tkuk:tkuk@cluster0.lkam2.mongodb.net/sample_mflix"
+
     try {
       await mongoose.connect(uri)
       console.log("db connected")
